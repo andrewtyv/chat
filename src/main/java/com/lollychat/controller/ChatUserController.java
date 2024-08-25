4a4f0289a398d201b4b5e5266237158c25fe0560
andrewtyv <132301547+andrewtyv@users.noreply.github.com>
2024-08-25 15:02:19 +0200

    jakarta

--- src/main/java/com/lollychat/controller/ChatUserController.java
+++ src/main/java/com/lollychat/controller/ChatUserController.java
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/user")
@@ -25,6 +25,7 @@ public class ChatUserController {
 
     @Autowired
     private JwtUtil jwtUtil;
+
 
     @GetMapping("/me")
     public ResponseEntity<ChatUserDTO> info(HttpServletRequest request){
