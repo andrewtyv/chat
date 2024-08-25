2bdc2ab7eca466670524b986562c7bff8dd1bfa4
andrewtyv <132301547+andrewtyv@users.noreply.github.com>
2024-08-25 15:32:40 +0200

    key_Signatyre

--- src/main/java/com/lollychat/securingweb/JwtUtil.java
+++ src/main/java/com/lollychat/securingweb/JwtUtil.java
@@ -5,13 +5,15 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final SignatureAlgorithm key_signatyre = SignatureAlgorithm.HS256;
+    private static final Key key = Keys.secretKeyFor(key_signatyre);
     private final long expiration = 86400000L; // 24 години
 
     public String generateToken(String username) {
